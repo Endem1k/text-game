8427d14a51c14980144437baf4d136fb6fee677e
Endem1k <gogelole48@gmail.com>
2017-11-06 18:42:00 +0300

    shs

--- src/com/banana/textgame/Main.java
+++ src/com/banana/textgame/Main.java
@@ -13,18 +13,22 @@ public class Main {
         // вызывает метод start()
         new Main().start();
     }
+
     /*
      * Метод с логикой игры.
      */
     private void start() {
-        onFinish();
+
         onStart();
-        for (int i = 1; i <= 5; ++i) {
-            onNewDay(i);
+        for (int e = 1; e <= 31; e = e + 2) {
+
+            onFinish(e);
+
         }
 
-        onStartP();
+
     }
+
     /*
      * Метод вызывается один раз при старте игры.
      */
@@ -35,12 +39,14 @@ public class Main {
         System.out.println("ПОШЕЛ ВОН!ВИДЕТЬ ТЕБЯ НЕ ХОЧУ!ЗАКРОЙ ЭТО ОКНО,БЫСТРО!" + ответ);
 
     }
+
     int индус = 0;
+
     /*
      * Метод вызывается каждый игровый день.ЖЗШ
      * Единственный параметр: dayNumber - номер текущеvго игрового дня.
      */
-    void onNewDay(int dayNumber) {
+   /* void onNewDay(int dayNumber) {
         System.out.println("День номер" + dayNumber + "");
         Scanner па = new Scanner(System.in);
         System.out.println("Ваше действие");
@@ -55,7 +61,7 @@ public class Main {
             } else {
                 System.out.println("Еще раз, я купил это дешевое кофе");
             }
-        } else if(action.equals("код")) {
+        } else if (action.equals("код")) {
             System.out.println("Ваш код на сегодня");
             String код = па.nextLine();
             индус = индус + код.length();
@@ -68,29 +74,70 @@ public class Main {
         boolean endsWithSpaces = код.endsWith(" ");
         boolean result = startsWithSpaces && endsWithSpaces;
         System.out.println("Халявщик: " + result);
-       */
+
 
     }
 
-    void onStartP() {
+  /*  void onStartP(int dayNumber2) {
+        System.out.println("День номер" + dayNumber2 + "");
+        Scanner па = new Scanner(System.in);
+        System.out.println("Ваше действие");
+        String action = па.nextLine();
+        switch (action) {
+            case "код":
+                System.out.println("Ваш код на сегодня");
+                String код = па.nextLine();
+                индус = индус + код.length();
+                System.out.println("Бабло Индуса:" + dayNumber2 + "$.");
+            break;
+            case "кофе":
+                индус -= 2;
+                System.out.println("Минус деньги, которые я мог потратить на еду");
+            break;
+            default:
+                System.out.println("Че это такое?");
+        }
+    }
+ */
 
-
-}
     /*
      * Метод вызывается по завершению игры.
      */
 
 
 
-    void onFinish() {
-        String launchGame = ("1,2,3");
-        Scanner па = new Scanner(System.in);
-        String age2 = па.nextLine();
-         if (int age = 15; age <= 13;) {
+    void onFinish(int dayNumber2) {
+        System.out.println("День номер" + dayNumber2 + "");
 
-                System.out.println("Не наш калибр...");
-         } else {
-             System.out.println( launchGame );
+        String dollarString = "";
+        for (int i = 0; i < индус ; i += 1) {
+            dollarString = dollarString + "$";
+        }
+        System.out.println("Вашм бабосики: " + dollarString + ".");
+
+        Scanner па = new Scanner(System.in);
+        System.out.println("Ваше действие");
+        String action = па.nextLine();
+        switch (action) {
+            case "код":
+
+                System.out.println("Ваш код на сегодня");
+                String код = па.nextLine();
+                индус = индус + код.length();
+                System.out.println("Бабло Индуса:" + индус + "$.");
+                break;
+            case "кофе":
+                if (Math.random() > 0.5) {
+                    индус -= 2;
+                    System.out.println("Минус деньги, которые я мог потратить на еду");
+
+
+                } else {
+                    System.out.println("Еще раз, я купил это дешевое кофе");
+                }
+                break;
+            default:
+                System.out.println("Че это такое?");
         }
     }
 
